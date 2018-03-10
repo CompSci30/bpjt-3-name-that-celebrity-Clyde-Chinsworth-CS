1f707f8876f9982b3b4eb8c8849147103f9bc946
Alex Yang <a.yang2@share.epsb.ca>
2018-03-10 19:02:29 +0000

    Declared strings

--- NameThatCelebrity.java
+++ NameThatCelebrity.java
@@ -4,6 +4,10 @@ public class NameThatCelebrity
 {
 	public static void main(String args[])
 	{
-	
+		String s1 = "Allan Alda";
+		String s2 = "John Wayne";
+		String s3 = "Gregory Peck";
+		
+			
 	}
 }
